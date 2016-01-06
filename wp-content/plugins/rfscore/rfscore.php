60f636c0008055bb476495ca83a3c6f95bc4365a
Runar Flåten <runar@tuta.io>
2016-01-06 12:38:48 +0100

    Adding metaboxes

---
+++ wp-content/plugins/rfscore/rfscore.php
@@ -0,0 +1,35 @@
+<?php
+/*
+Plugin Name: RF score
+Plugin URI:
+Description: Show score on website
+Version: 2.0
+Author: Runar Flåten
+Author URI: runar.xyz
+License: GPLv2
+*/
+
+add_action( 'add_meta_boxes', 'rf_score_register_meta_box');
+
+function rf_score_register_meta_box() {
+  add_meta_box(
+    'rf-score-meta',
+    'Score',
+    'rf_score_meta_box',
+    'post',
+    'side',
+    'high'
+  );
+
+  add_meta_box(
+    'rf-goals-meta',
+    'Goals',
+    'rf_goals_meta_box',
+    'post',
+    'side',
+    'high'
+  );
+}
+
+
+ ?>
